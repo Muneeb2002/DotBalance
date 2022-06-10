663e64f8486434cedbd286faed22fcbbd44fa762
Muneeb2002 <mmuneebfar@gmail.com>
2022-06-10 19:19:16 +0200

    fået lidt bevægelse(transfer)

--- marblerunner/lib/main.dart
+++ marblerunner/lib/main.dart
@@ -6,6 +6,10 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
 import 'package:sensors_plus/sensors_plus.dart';
+// import 'package:flame/image.dart';
+import 'package:flame/flame.dart';
+import 'package:flame/sprite.dart';
+
 // import
 
 BallGame ballGame = BallGame();
@@ -13,11 +17,13 @@ BallGame ballGame = BallGame();
 final paint = BasicPalette.red.paint()..style = PaintingStyle.fill;
 final circle = CircleComponent(
     radius: 200.0,
-    position: Vector2(100, 200),
+    position: Vector2(10, 10),
     paint: paint,
     anchor: Anchor.center);
 
 TextPaint textPaint = TextPaint();
+
+SpriteComponent background = SpriteComponent();
 
 void main() {
   runApp(HomeWidget());
@@ -36,17 +42,16 @@ class _HomeWidgetState extends State<HomeWidget> {
     return MaterialApp(
         home: Scaffold(
             body: Container(
-      child: GameWidget(game: BallGame()),
-    ),
-    floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            circle.position = Vector2(200,200);
-          });
-        },
-        child: const Text("recalibrate"),
-      )
-    ));
+              child: GameWidget(game: BallGame()),
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                setState(() {
+                  // background.position = Vector2(0,0);
+                });
+              },
+              child: const Text("recalibrate"),
+            )));
   }
 }
 
@@ -55,22 +60,41 @@ class gyro extends _HomeWidgetState {
   void initState() {
     super.initState();
     gyroscopeEvents.listen((GyroscopeEvent event) {
-      BallGame.move([event.y, event.x, event.z]);
+      ballGame.move([event.y, event.x, event.z]);
     });
   }
 }
 
-class BallGame extends FlameGame with TapDetector {
+class BallGame extends FlameGame {
   @override
-  Color backgroundColor() => Colors.blue;
+  Color backgroundColor() => Colors.pink;
   @override
   Future<void> onLoad() async {
+    loadPictures();
     add(circle);
     double middelx = size[0] / 2;
     double middely = size[1] / 2;
 
     circle.position = Vector2(middelx, middely);
     circle.size = Vector2(20, 20);
+  }
+
+  void loadPictures() async {
+    background = SpriteComponent()
+      ..sprite = await loadSprite('test.png')
+      ..size = Vector2(1000, 1000)
+      ..anchor = Anchor.center
+      ..position = Vector2(size[0] / 2, size[1] / 2);
+
+    add(background);
+
+    SpriteComponent ball = SpriteComponent()
+      ..sprite = await loadSprite('ball.png')
+      ..size = Vector2(50, 50)
+      ..anchor = Anchor.center
+      ..position = Vector2(size[0] / 2, size[1] / 2);
+
+    add(ball);
   }
 
   @override
@@ -84,15 +108,11 @@ class BallGame extends FlameGame with TapDetector {
     super.render(canvas);
   }
 
-  @override
-  void onTap() {
-    super.onTap();
-    circle.position.x += 10;
-  }
-
-  static void move(List gyro) {
+  void move(List gyro) {
     circle.position.x += gyro[0] / 10;
     circle.position.y += gyro[1] / 10;
+    background.position.x -= gyro[0] / 10;
+    background.position.y -= gyro[1] / 10;
   }
 
   // List getGyro() {
